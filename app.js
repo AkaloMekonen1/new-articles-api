cc36b23c18c2e8ebc9b4d119f0b4472e22ac9889
Akalo Mekonen <mekonen168@gmail.com>
2020-10-02 20:25:44 +0300

    I have created new Middleware that handles all the errors

--- app.js
+++ app.js
@@ -17,9 +17,17 @@ app.get('/', (req, res)=>{
 })
 
 app.use((req, res, next) =>{
-   const error = new Error("Not Found")
+   const error = new Error('Not Found')
    error.status = 404
    next(error)
+})
+app.use((error,req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
 })
 
 module.exports = app
