105019789b7da783c9259c5bf74893b8de6194b8
Akalo Mekonen <mekonen168@gmail.com>
2020-10-02 20:04:47 +0300

    if the request is OPTION send header Access-Control-Allow-Method

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept, Authorization")
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+    }
 })
 app.get('/', (req, res)=>{
     res.status(200).json({
