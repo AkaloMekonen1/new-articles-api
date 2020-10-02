0169f3d8dba85ecf667260ecd8e72cbc669ae75e
Akalo Mekonen <mekonen168@gmail.com>
2020-10-02 20:00:12 +0300

    second header added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) =>{
-    res.header()
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept, Authorization")
 })
 app.get('/', (req, res)=>{
     res.status(200).json({
