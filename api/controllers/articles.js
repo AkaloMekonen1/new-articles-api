badc75381cc4f297ad0eecd9289e34a00ab2b98e
Akalo Mekonen <mekonen168@gmail.com>
2020-10-05 21:47:23 +0300

    I am taking the article user sent using POST by accessing to req.body with title description and content

--- api/controllers/articles.js
+++ api/controllers/articles.js
@@ -5,6 +5,7 @@ module.exports = {
         })
     },
     postAllArticles : (req, res) =>{
+        const { title, description, content } = req.body
         res.status(200).json({
             message: 'Create All Articles'
         })
