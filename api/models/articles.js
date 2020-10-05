d1df4a780b0cf8311803969cdadb9f15b98d05a5
Akalo Mekonen <mekonen168@gmail.com>
2020-10-05 22:12:27 +0300

    I have added id to Schema object this will be an unique id for each article

--- api/models/articles.js
+++ api/models/articles.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const articleSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    content: String
+    _id: mongoose.Schema.type.objectID,
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    content: {type: String, required: true}
 })
