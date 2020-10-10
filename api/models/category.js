76d12e33482fd4f402f186a103cb9380ca508ed9
Akalo Mekonen <mekonen168@gmail.com>
2020-10-10 14:45:38 +0300

    I have changed the model name to Category

--- api/models/category.js
+++ api/models/category.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const articleSchema = mongoose.Schema({
+const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectID,
     title: {type: String, required: true},
     description: {type: String, required: true},
     content: {type: String, required: true}
 })
 
-module.exports = mongoose.model('Article', articleSchema)
+module.exports = mongoose.model('Category', articleSchema)
