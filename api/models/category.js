a71d1eea60e49fdeeadc7a3ac4648dc6ae5b8e9e
Akalo Mekonen <mekonen168@gmail.com>
2020-10-10 15:27:20 +0300

    syntax error fixed

--- api/models/category.js
+++ api/models/category.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectID,
     title: {type: String, required: true},
-    description: {type: String, required: true},
-    content: {type: String, required: true}
+    description: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Category', categorySchema)
