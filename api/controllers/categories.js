94af2b0f2dc11619a825831d3dcb66495bcfc07e
Akalo Mekonen <mekonen168@gmail.com>
2020-10-10 15:00:15 +0300

    I have copied from article controllers the body of getAllArticles and PostAllArticles and amended it correspondingly

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -1,12 +1,35 @@
+const Category = require('../models/category')
+const mongoose = require('mongoose')
+const { json } = require('express')
 module.exports = {
     getAllCategories: (req, res) =>{
-        res.status(200).json({
-            message: 'Get All Categories'
+        
+        Category.find().then((categories)=>{
+            res.status(200).json({
+                categories
+            })
+        }).catch(error =>{
+            res.status(500).json({
+                error
+            })
         })
     },
     postAllCategory : (req, res) =>{
-        res.status(200).json({
-            message: 'Create All Category'
+        
+        const { title, description } = req.body
+        const category = new Category({
+            _id: new mongoose.Types.ObjectId,
+            title,
+            description
+        })
+        category.save().then(()=>{
+            res.status(200).json({
+                message: 'Created Category'
+            })
+        }).catch(error =>{
+            res.status(500).json({
+                error
+            })
         })
     },
     patchAllCategory: (req, res) =>{
