7fa1d2f93b83624c3e71ebf0f4ad260b758ba339
Akalo Mekonen <mekonen168@gmail.com>
2020-10-10 13:27:24 +0300

    I have created a new router getArticle

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getAllArticles, postAllArticles, patchAllArticles, deleteAllArticles} = require('../controllers/articles')
+const { getAllArticles, postAllArticles, patchAllArticles, deleteAllArticles, getArticle} = require('../controllers/articles')
 
 router.get('/', getAllArticles)
 router.post('/', postAllArticles)
+router.get('/:articleId', getArticle)
 router.patch('/:articleId', patchAllArticles)
 router.delete('/:articleId', deleteAllArticles)
 
