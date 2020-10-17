a13422519de81a8acd5b1c91ddc76502bcad19d1
Akalo Mekonen <mekonen168@gmail.com>
2020-10-17 16:21:41 +0300

    I have limited the size of image that can be uploaded up to 2GB

--- api/middlewares/upload.js
+++ api/middlewares/upload.js
@@ -1,7 +1,10 @@
 const multer = require('multer')
 
 const upload = multer({
-    dest: 'uploads/'
+    dest: 'uploads/',
+    limits:{
+        fileSize: 1024 * 1024 * 2
+    }
 })
 
 module.exports = upload
