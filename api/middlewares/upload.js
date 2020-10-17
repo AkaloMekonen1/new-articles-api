b9ed9dbde9807901884476ef57a733a176d6ef81
Akalo Mekonen <mekonen168@gmail.com>
2020-10-17 16:28:38 +0300

    I created condition that only omages that are jpeg or ong can be uploaded

--- api/middlewares/upload.js
+++ api/middlewares/upload.js
@@ -1,10 +1,17 @@
 const multer = require('multer')
 
+const fileFilter = (req, file, cb)=>{
+    if(file.mimetype === 'image.png' || file.mimetype === 'image.jpeg'){
+        cb(null, true)
+    }
+    cb(null, false)
+}
 const upload = multer({
     dest: 'uploads/',
     limits:{
         fileSize: 1024 * 1024 * 2
-    }
+    },
+    fileFilter
 })
 
 module.exports = upload
