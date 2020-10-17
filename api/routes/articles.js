7b48c0d6599d7cf5081b35d698c1130b44624ab6
Akalo Mekonen <mekonen168@gmail.com>
2020-10-17 15:21:31 +0300

    I added the middelware upload to te router post in article routes

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { getAllArticles, postAllArticles, patchAllArticles, deleteAllArticles, getArticle} = require('../controllers/articles')
+const upload = require('../middlewares/upload')
 
 router.get('/', getAllArticles)
-router.post('/', postAllArticles)
+router.post('/', upload, postAllArticles)
 router.get('/:articleId', getArticle)
 router.patch('/:articleId', patchAllArticles)
 router.delete('/:articleId', deleteAllArticles)
