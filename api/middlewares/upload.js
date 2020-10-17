af246b67b77a29a07ff57e5f6a455a6c3dab0120
Akalo Mekonen <mekonen168@gmail.com>
2020-10-17 23:26:23 +0300

    I have replaced the destination with storage

--- api/middlewares/upload.js
+++ api/middlewares/upload.js
@@ -1,5 +1,13 @@
 const multer = require('multer')
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb ) =>{
+        cb(null, `${Date.now()} -${file.originalname}`)
+    }
+})
 const fileFilter = (req, file, cb)=>{
     if(file.mimetype === 'image.png' || file.mimetype === 'image.jpeg'){
         cb(null, true)
@@ -7,7 +15,7 @@ const fileFilter = (req, file, cb)=>{
     cb(null, false)
 }
 const upload = multer({
-    dest: 'uploads/',
+    storage,
     limits:{
         fileSize: 1024 * 1024 * 2
     },
